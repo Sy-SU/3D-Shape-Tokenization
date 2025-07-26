77236f590a1676c1b21f93d5ee96a13bdab4e86a
Senyang Su <susenyang@163.com>
2025-07-26 23:11:24 +0800

    add simple test eval

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+# utils/__init__.py
+
+from .utils import FourierPositionalEncoding3D, PointFeatureProjector, RMSNorm
+
+__all__ = [
+    "FourierPositionalEncoding3D",
+    "PointFeatureProjector",
+    "RMSNorm"
+]
